7caad9de7f78e7ec8928692b613289c7b9654fc7
DmitryMishchenko <64646382+AzarakD@users.noreply.github.com>
2022-08-07 16:21:35 +0400

    add list of goods

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { useEffect, useState } from "react";
+
+import Preloader from "./Preloader";
+import Goods from "./Goods";
+import { API_KEY, API_URL } from "../config";
 
 const Shop = () => {
-  return <main className="container content">Shop</main>;
+  const [goods, setGoods] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(API_URL, {
+      headers: {
+        Authorization: API_KEY,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        data.featured && setGoods(data.featured);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <main className="container content">
+      {loading ? <Preloader /> : <Goods goods={goods} />}
+    </main>
+  );
 };
 
 export default Shop;
